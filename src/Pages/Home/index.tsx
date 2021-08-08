fe0401b870c26146a3caed8dda7331206d2483c5
Felipe Catete <catetedev@gmail.com>
2021-08-08 07:24:26 -0300

    Add files via upload

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "./styles.css";
 
+// IMAGENS
+// símbolo da app store
+import appstore from "../../Images/Home/app-store-logo.png";
+// símbolo da google play
+import googleplay from "../../Images/Home/google-play-logo.png";
+// qr-code
+import qrcode from "../../Images/Home/qr-code.png";
+
 // REACT-ICONS
 // círculo azul
 import { HiCheckCircle } from "react-icons/hi";
@@ -12,9 +20,9 @@ function Home() {
     return (
         <section className="home_section1">
             <div className="home_section1_main_div">
-                <div>
+                <div className="home_h1_and_list">
                     <div className="home_h1_div">
-                        <h1>The World´s Fastest Growing<br />Crypto App</h1>
+                        <h1>The World’s Fastest Growing Crypto App</h1>
                     </div>
                     <div className="home_text_div">
                         <div className="home_div_ul">
@@ -40,11 +48,17 @@ function Home() {
                     </div>
                 </div> {/* engloba a div do h1 e da lista nessa div*/}
 
-                <div>
-                    <img />
-                    <img />
-                    <img />
-                </div> {/* contém os síbolos do Google Play e App Store */}
+                <div className="home_logo_div">
+                    <a href="#">
+                        <img className="home_appstore" src={appstore} />
+                    </a>
+                    <a href="#">
+                        <img className="home_googleplay" src={googleplay} />
+                    </a>
+                    <a href="#">
+                        <img className="home_qrcode" src={qrcode} />
+                    </a>
+                </div> {/* contém os símbolos do Google Play e App Store */}
             </div>
         </section>
     )
